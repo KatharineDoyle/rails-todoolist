2547792ce9bea6c3dd8ed57eae84b279fa2ccc49
Katharine Doyle <katharinecdoyle@gmail.com>
2019-01-27 18:25:44 +0000

    Added CRUD on tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,36 @@
 class TasksController < ApplicationController
 
   def index
-    @task = Task.all
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
   end
 
   def new
     @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to tasks_path
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path(@task)
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :details, :completed)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_27_162146) do
+ActiveRecord::Schema.define(version: 2019_01_27_171403) do
 
   create_table "tasks", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+    t.boolean "completed"
+    t.string "details"
   end
 
 end
